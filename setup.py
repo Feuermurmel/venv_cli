8e16ed366c6c97787814508121b0bfff33e9095a
Michael Schwarz <michi.schwarz@gmail.com>
2016-02-17 17:03:12 +0100

    Remove package_data to prevent strange errors.
    It seems that setuptools tried to package up *.pyc or .DS_Store files, and failed.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setuptools.setup(
 	name = 'venv_cli',
 	version = '0.1',
 	packages = setuptools.find_packages(),
-	package_data = { '': ['*.*'] },
 	install_requires = [],
 	entry_points = dict(
 		console_scripts = [
