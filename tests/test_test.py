1887d957d18e75cda3f444c07dea61fc6689f05f
Michael Schwarz <michi.schwarz@gmail.com>
2016-02-25 23:08:08 +0100

    Test for version string on older python versions.

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,3 +1,4 @@
+import shutil
 from .helpers import *
 
 
@@ -33,14 +34,9 @@ def test_test_venv():
 	"""
 	
 	with workspace(virtualenvs = ['venv']) as ws:
-		blurb = 'is a virtualenv running'
-		
-		res = ws.run('venv --test')
-		
-		assert blurb in res.stderr
-		
-		# Check that we didn't get an empty version string. This is brittle and should probably be replaced by something more robust.
-		assert blurb + ' .' not in res.stderr
+		ws.run(
+			'venv --test',
+			expect_stderr_contains = 'is a virtualenv running')
 
 
 def test_test_venv_different_name():
@@ -52,3 +48,29 @@ def test_test_venv_different_name():
 		ws.run(
 			'venv --test venv2',
 			expect_stderr_contains = 'is a virtualenv running')
+
+
+def test_test_old_python_version():
+	"""
+	Test whether the version string can be extracted from different Python versions.
+	"""
+	
+	def filter_versions(names):
+		return [i for i in names if shutil.which(i) is not None]
+	
+	old_behavior_versions = filter_versions(['python2.6', 'python2.7', 'python3.0', 'python3.1', 'python3.2', 'python3.3'])
+	new_behavior_versions = filter_versions(['python3.4', 'python3.5', 'python3.6', 'python3.7', 'python3.8', 'python3.9'])
+	
+	if not old_behavior_versions and not new_behavior_versions:
+		pytest.skip('A python version <= 3.3 and another >= 3.4 is necessary.')
+	
+	for i in old_behavior_versions[0], new_behavior_versions[0]:
+		with workspace() as ws:
+			ws.run('venv -c -p {}'.format(i))
+			
+			res = ws.run('venv --test')
+			
+			assert 'is a virtualenv running' in res.stderr
+			
+			# Check that we didn't get an empty version string. This is brittle and should probably be replaced by something more robust.
+			assert 'is a virtualenv running .' not in res.stderr
